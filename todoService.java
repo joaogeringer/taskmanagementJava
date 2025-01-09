dc75421e2efa677528742bdd1bdc50319d34b172
João Geringer <91030587+joaogeringer@users.noreply.github.com>
2025-01-09 17:57:43 -0300

    Create todoService.java

---
+++ todoService.java
@@ -0,0 +1,51 @@
+package com.joaogeringer.taskmanagement.service;
+import java.util.List;
+import java.util.Properties;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import com.joaogeringer.taskmanagement.entity.todo;
+import com.joaogeringer.taskmanagement.repository.TodoRepository;
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<todo> list() {
+      Sort sort = Sort.by(...Properties:"prioridade").descending().and(
+        Sort.by(...Properties:"nome").ascending());
+        return todoRepository.findAll(sort);
+    }
+
+    public List<todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+    
+    public List<todo> delete() {
+        todoRepository.deleteById(id);
+        return list();
+    }
+
+    private Object descending() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void ascending() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+
+}
+
+
+
